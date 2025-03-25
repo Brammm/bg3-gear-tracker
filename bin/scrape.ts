8794bf9cf791f4ab29423f6a714a9fdcbde234fd
Bram Van der Sype <bram.vandersype@gmail.com>
2025-03-25 13:44:54 +0100

    Parse item rarity

--- bin/scrape.ts
+++ bin/scrape.ts
@@ -17,7 +17,8 @@ type EquipmentType = {
 
 type Item = {
     name: string;
-    // rarity: Rarity;
+    url: string;
+    rarity: Rarity;
     thumbnail?: string;
 }
 
@@ -30,7 +31,7 @@ async function parseEquipment() {
         promises.push(parseEquipmentType($, elem));
     });
 
-    return (await Promise.all(promises)).filter((type) => type != undefined);
+    return (await Promise.all(promises)).filter((type) => type !== undefined);
 }
 
 async function parseItems(url: string): Promise<Item[]> {
@@ -38,34 +39,55 @@ async function parseItems(url: string): Promise<Item[]> {
     const $ = cheerio.load(html.data);
 
     const items: Item[] = [];
-    const promises: Promise<Item>[] = [];
+    const promises: Promise<Item | undefined>[] = [];
     $('table.wikitable').each((_, table) => {
         $('tbody tr', table).each((_, elem) => {
             const cell = $('td:first', elem);
-            promises.push(parseItem($, cell, url));
+            promises.push(parseItem($, cell, path.basename(url)));
         });
     });
-    items.push(...await Promise.all(promises));
+    items.push(...(await Promise.all(promises)).filter((item) => item !== undefined));
 
     return items;
 }
 
-async function parseItem($: cheerio.Root, elem: cheerio.Cheerio, url: string): Promise<Item> {
+async function parseItem($: cheerio.Root, elem: cheerio.Cheerio, type: string): Promise<Item | undefined> {
     const link = $('p > a', elem);
     const thumbnail = $('img', elem);
 
+    const url = link.attr('href');
+    if (!url) {
+        return;
+    }
+
     const item: Item = {
-        name: link.text(),
-        // rarity,
+        name: link.text().trim(),
+        url,
+        rarity: await parseRarity(url),
     };
 
     // download thumbnail
     const src = thumbnail.attr('src');
     if (src) {
-        item.thumbnail = await parseThumbnail(src, 'thumbs/' + path.basename(url));
+        item.thumbnail = await parseThumbnail(src, 'thumbs/' + type);
     }
 
     return item;
+}
+
+async function parseRarity(url: string): Promise<Rarity> {
+    const html = await axios.get(url);
+    const $ = cheerio.load(html.data);
+
+    const properties = $('.bg3wiki-property-list').text();
+
+    const rarityMatch = properties.match(/^\s*Rarity:\s*([A-Za-z\s]+)$/m);
+
+    if (!rarityMatch) {
+        return 'Common';
+    }
+
+    return rarityMatch.length > 0 ? rarityMatch[1] as Rarity : 'Common';
 }
 
 async function parseEquipmentType($: cheerio.Root, elem: cheerio.Element): Promise<EquipmentType | undefined> {
@@ -79,7 +101,7 @@ async function parseEquipmentType($: cheerio.Root, elem: cheerio.Element): Promi
     }
 
     const type: EquipmentType = {
-        name: link.text(),
+        name: link.text().trim(),
         url,
         items: await parseItems(url),
     };
@@ -113,11 +135,8 @@ async function parseThumbnail(src: string, basePath: string): Promise<string> {
     return fullPath;
 }
 
-const items = await parseEquipment();
+const equipment = await parseEquipment();
 
-console.log(items);
+fs.writeFileSync('thumbs/data.json', JSON.stringify(equipment, null, 2));
 
-
-// const equipment = await parseEquipment();
-//
-// console.log(equipment);
+console.log('done');
