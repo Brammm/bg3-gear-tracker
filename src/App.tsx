486406bb061e84be2e355746f7e330187e2230d2
Bram Van der Sype <bram.vandersype@gmail.com>
2025-04-01 13:30:27 +0200

    Add item combobox

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import equipment from './data/equipment.json';
+import ItemCombobox from './components/item-combobox.tsx';
+
 function App() {
     return (
         <>
             <h1>Hello World!</h1>
+            {equipment.map((type) => (
+                <div key={type.name}>
+                    <h2 className="flex items-center">
+                        <img src={type.thumbnail} alt={type.name}  className="size-6 shrink-0" />
+                        <span className="ml-3">{type.name}</span>
+                    </h2>
+                    <ItemCombobox items={type.items} />
+                </div>
+            ))}
         </>
     );
 }
