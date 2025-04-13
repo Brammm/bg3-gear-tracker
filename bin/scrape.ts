66f3711d0c0f40d4cefab9cb9f4a9080c6a48e52
Bram Van der Sype <bram.vandersype@gmail.com>
2025-04-13 15:19:36 +0200

    Improve scrape command
    - Make requests sequentially to avoid overloading wiki
    - Group armour and clothing
    - split melee and ranged weapons
    - Store equipment as typed object instead of json

--- bin/scrape.ts
+++ bin/scrape.ts
@@ -2,152 +2,172 @@ import * as fs from "node:fs";
 import * as path from "node:path";
 import axios from "axios";
 import * as cheerio from "cheerio";
+import {
+    type EquipmentType,
+    type Item,
+    type Rarity,
+    rarityColorMap,
+} from "../src/data/rarity.ts";
 
 axios.defaults.baseURL = "https://bg3.wiki";
+const parsedItems: string[] = [];
 
-const rarities = [
-    "Common",
-    "Uncommon",
-    "Rare",
-    "Very rare",
-    "Legendary",
-] as const;
-type Rarity = (typeof rarities)[number];
+// Base configuration
+const equipment: EquipmentType[] = [
+    {
+        name: "Amulets",
+        url: "/wiki/Amulets",
+        thumbnail: "/equipment/120px-Keepsake_Locket_A_Unfaded.png",
+        items: [],
+    },
+    {
+        name: "Armour",
+        url: "/wiki/Armour",
+        thumbnail: "/equipment/120px-Padded_Armour_Unfaded.png",
+        items: [],
+    },
+    {
+        name: "Cloaks",
+        url: "/wiki/Cloaks",
+        thumbnail: "/equipment/120px-Cloak_Long_C_1_Unfaded.png",
+        items: [],
+    },
+    {
+        name: "Footwear",
+        url: "/wiki/Footwear",
+        thumbnail: "/equipment/120px-Boots_Leather_Unfaded.png",
+        items: [],
+    },
+    {
+        name: "Headwear",
+        url: "/wiki/Headwear",
+        thumbnail: "/equipment/120px-Circlet_of_Mental_Anguish_Unfaded.png",
+        items: [],
+    },
+    {
+        name: "Handwear",
+        url: "/wiki/Handwear",
+        thumbnail: "/equipment/120px-Gloves_Metal_Unfaded.png",
+        items: [],
+    },
+    {
+        name: "Rings",
+        url: "/wiki/Rings",
+        thumbnail: "/equipment/120px-Crushers_Ring_Unfaded.png",
+        items: [],
+    },
+    {
+        name: "Shields",
+        url: "/wiki/Shields",
+        thumbnail: "/equipment/120px-Studded_Shield_Unfaded.png",
+        items: [],
+    },
+    {
+        name: "Melee Weapons",
+        url: "/wiki/List_of_melee_weapons",
+        thumbnail: "/equipment/120px-Greataxe_Unfaded.png",
+        items: [],
+    },
+    {
+        name: "Ranged Weapons",
+        url: "/wiki/List_of_ranged_weapons",
+        thumbnail: "/equipment/50px-Longbow_Unfaded_Icon.png",
+        items: [],
+    },
+];
 
-const items: string[] = [];
-
-type EquipmentType = {
-    name: string;
-    url: string;
-    thumbnail?: string;
-    items: Item[];
-};
-
-type Item = {
-    name: string;
-    url: string;
-    rarity: Rarity;
-    thumbnail?: string;
-};
-
-async function parseEquipment() {
-    const html = await axios.get("/wiki/Equipment");
-    const $ = cheerio.load(html.data);
-
-    const promises: Promise<EquipmentType | undefined>[] = [];
-    $(".gallery li > div").each(async (_, elem) => {
-        promises.push(parseEquipmentType($, elem));
-    });
-
-    return (await Promise.all(promises)).filter((type) => type !== undefined);
+// clean up old files/data
+if (fs.existsSync("src/data/equipment.json")) {
+    fs.rmSync("src/data/equipment.json");
 }
+if (fs.existsSync("public/thumbs")) {
+    fs.rmSync("public/thumbs", { recursive: true });
+}
+
+// loop over equipment types
+for await (const type of equipment) {
+    console.log(`Parsing ${type.name}`);
+
+    const items = await parseItems(type.url);
+    for await (const item of items) {
+        item.thumbnail = await downloadThumbnail(
+            item.thumbnail,
+            `public/thumbs/${type.name}`,
+        );
+    }
+
+    type.items = items;
+}
+
+// write parsed data to file
+fs.writeFileSync(
+    "src/data/equipment.ts",
+    `import type { EquipmentType } from './rarity';
+
+    export const equipment: EquipmentType[] = ${JSON.stringify(equipment, null, 4)}; \n`,
+);
+
+console.log("Done");
+
+///////////////////////// FUNCTIONS
 
 async function parseItems(url: string): Promise<Item[]> {
     const html = await axios.get(url);
     const $ = cheerio.load(html.data);
 
     const items: Item[] = [];
-    const promises: Promise<Item | undefined>[] = [];
     $("table.wikitable").each((_, table) => {
         $("tbody tr", table).each((_, elem) => {
             const cell = $("td:first", elem);
-            promises.push(parseItem($, cell, path.basename(url)));
+
+            const link = $("p > a", cell);
+            const thumbnail = $("img", cell);
+
+            const url = link.attr("href");
+            const src = thumbnail.attr("src");
+
+            // Don't add item if no url or image or already parsed
+            if (!url || parsedItems.includes(url) || !src) {
+                return;
+            }
+
+            items.push({
+                name: link.text().trim(),
+                url,
+                rarity: parseRarity($, link),
+                thumbnail: src,
+            });
+            parsedItems.push(url);
         });
     });
-    items.push(
-        ...(await Promise.all(promises)).filter((item) => item !== undefined),
-    );
 
     return items;
 }
 
-async function parseItem(
-    $: cheerio.Root,
-    elem: cheerio.Cheerio,
-    type: string,
-): Promise<Item | undefined> {
-    const link = $("p > a", elem);
-    const thumbnail = $("img", elem);
+function parseRarity($: cheerio.Root, link: cheerio.Cheerio): Rarity {
+    const span = $("span", link);
 
-    const url = link.attr("href");
-    if (!url || items.includes(url)) {
-        return;
+    const styleAttr = span.attr("style") || "";
+
+    const colorMatch = styleAttr.match(/color:\s*([^;]+)/);
+
+    if (colorMatch) {
+        const foundColor = colorMatch[1].trim();
+
+        return (
+            (Object.entries(rarityColorMap).find(
+                ([_, color]) => foundColor === color,
+            )?.[0] as Rarity) || "Common"
+        );
     }
 
-    items.push(url);
-
-    const item: Item = {
-        name: link.text().trim(),
-        url,
-        rarity: await parseRarity(url),
-    };
-
-    // download thumbnail
-    const src = thumbnail.attr("src");
-    if (src) {
-        item.thumbnail = await parseThumbnail(src, `public/thumbs/${type}`);
-    }
-
-    return item;
+    return "Common";
 }
 
-async function parseRarity(url: string): Promise<Rarity> {
-    const html = await axios.get(url);
-    const $ = cheerio.load(html.data);
-
-    const properties = $(".bg3wiki-property-list").text();
-
-    const rarityMatch = properties.match(/^\s*Rarity:\s*([A-Za-z\s]+)$/m);
-
-    if (!rarityMatch) {
-        return "Common";
-    }
-
-    return rarityMatch.length > 0 ? (rarityMatch[1] as Rarity) : "Common";
-}
-
-async function parseEquipmentType(
-    $: cheerio.Root,
-    elem: cheerio.Element,
-): Promise<EquipmentType | undefined> {
-    const link = $(".gallerytext p a", elem);
-    const thumbnail = $(".thumb img", elem);
-
-    const url = link.attr("href");
-
-    if (!url) {
-        return;
-    }
-
-    const items =
-        url === "/wiki/Weapon"
-            ? (
-                  await Promise.all([
-                      parseItems("/wiki/List_of_common_weapons"),
-                      parseItems("/wiki/List_of_uncommon_weapons"),
-                      parseItems("/wiki/List_of_rare_weapons"),
-                      parseItems("/wiki/List_of_very_rare_weapons"),
-                      parseItems("/wiki/List_of_legendary_weapons"),
-                  ])
-              ).flat()
-            : await parseItems(url);
-
-    const type: EquipmentType = {
-        name: link.text().trim(),
-        url,
-        items: items.sort((a, b) => a.url.localeCompare(b.url)),
-    };
-
-    // download thumbnail
-    const src = thumbnail.attr("src");
-    if (src) {
-        type.thumbnail = await parseThumbnail(src, "public/thumbs/Equipment/");
-    }
-
-    return type;
-}
-
-async function parseThumbnail(src: string, basePath: string): Promise<string> {
+async function downloadThumbnail(
+    src: string,
+    basePath: string,
+): Promise<string> {
     const filename = path.basename(src);
     const fullPath = path.join(basePath, filename);
 
@@ -170,18 +190,3 @@ async function parseThumbnail(src: string, basePath: string): Promise<string> {
 function decodeName(name: string): string {
     return name.replace("public", "").replaceAll("%2B", "+");
 }
-
-if (fs.existsSync("src/data/equipment.json")) {
-    fs.rmSync("src/data/equipment.json");
-}
-if (fs.existsSync("public/thumbs")) {
-    fs.rmSync("public/thumbs", { recursive: true });
-}
-
-const equipment = await parseEquipment();
-fs.writeFileSync(
-    "src/data/equipment.json",
-    `${JSON.stringify(equipment, null, 4)}\n`,
-);
-
-console.log("done");
---
+++ src/data/rarity.ts
@@ -0,0 +1,32 @@
+const rarities = [
+    "Common",
+    "Uncommon",
+    "Rare",
+    "Very rare",
+    "Legendary",
+    "Story Item",
+] as const;
+export type Rarity = (typeof rarities)[number];
+
+export const rarityColorMap: Record<Rarity, string> = {
+    Common: "#FFFFFF",
+    Uncommon: "#01BD39",
+    Rare: "#01BFFF",
+    "Very rare": "#D1017B",
+    Legendary: "#B7861D",
+    "Story Item": "#FF5901",
+};
+
+export type EquipmentType = {
+    name: string;
+    url: string;
+    thumbnail?: string;
+    items: Item[];
+};
+
+export type Item = {
+    name: string;
+    url: string;
+    rarity: Rarity;
+    thumbnail: string;
+};
